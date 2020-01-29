a0d3ffc0a2ee07ec0aec5ec1893f1ac3028cbebe
curro casado <currocuo9625@gmail.com>
2020-01-28 19:22:29 -0600

    Creating a custom error page with pug

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const createError = require("http-errors");
 const path = require("path");
+
 const app = express();
 
 app.set("view engine", "pug");
@@ -15,6 +17,19 @@ app.get("/favicon.ico", (req, res, next) => {
   return res.sendStatus(204);
 });
 app.use("/", routes());
+
+app.use((req, res, next) => {
+  return next(createError(404, "File not found"));
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  const status = err.status || 500;
+  res.locals.status = status;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(status);
+  return res.render("error");
+});
 
 app.listen(3000);
 
